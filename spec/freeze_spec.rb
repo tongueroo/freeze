6e5dbddfccc934787e090d48ee750fcc0d3af801
Tung Nguyen <tongueroo@gmail.com>
2019-07-08 02:22:32 +0000

    init commit

---
+++ spec/freeze_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Freeze do
+  it "has a version number" do
+    expect(Freeze::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
